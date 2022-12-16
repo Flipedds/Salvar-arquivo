5bfccadc39b753eed4eecda8814559cd16393761
Filipe André <110608654+Flipedds@users.noreply.github.com>
2022-12-16 11:31:46 +0000

    primeiro commit

--- main.py
+++ main.py
@@ -10,13 +10,3 @@ try:
 	pdf.output("otimizacao.pdf")    
 except:
 	print("não foi possível")
-
-'''
-	result =
-	
-	salvar_arquivo = input("qual nome deseja dar ao arquivo:\n")
-	salvar_arquivo += ".pdf"
-	arquivo = open(salvar_arquivo,'w')
-	arquivo.write(result) 
-	arquivo.close()
-  '''
